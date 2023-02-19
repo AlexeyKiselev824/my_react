438e391a034cc841870c2db268be550863cfc6ec
AlexeyKiselev824 <smoke824@yandex.ru>
2023-02-20 03:40:31 +0500

    Fixed a bug in the html markup of the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,11 @@ import LoginForm from '../components/loginForm/LoginForm';
 
 const Login = () => {
     return (
-        <div>
+        <>
             <Layout>
                 <LoginForm />
             </Layout>
-        </div>
+        </>
     )
 }
 
